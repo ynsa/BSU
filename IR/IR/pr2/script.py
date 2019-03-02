bddfe85ba732d87b94b076d95bac1ed882cc96be
Yana <fairytalespring@gmail.com>
2019-03-02 21:18:53 +0300

    Add 3grams. 11702/46636

--- IR/IR/pr2/script.py
+++ IR/IR/pr2/script.py
@@ -1,5 +1,6 @@
 import string
 import codecs
+from nltk import ngrams
 
 KEYBOARD_RU_EN = {'ё': '`', 'Ё': '~', '"': '@', '№': '#', ';': '$', ':': '^', '?': '&', 'й': 'q', 'Й': 'Q', 'ц': 'w', 'Ц': 'W', 'У': 'E', 'у': 'e', 'К': 'R', 'к': 'r', 'Е': 'T', 'е': 't', 'Н': 'Y', 'н': 'y', 'Г': 'U', 'г': 'u', 'Ш': 'I', 'ш': 'i', 'Щ': 'O', 'щ': 'o', 'З': 'P', 'з': 'p', 'Х': '{', 'х': '[', 'ъ': ']', 'Ъ': '}', '/': '|', '\\': '\\', 'Ф': 'A', 'ф': 'a', 'Ы': 'S', 'ы': 's', 'В': 'D', 'в': 'd', 'А': 'F', 'а': 'f', 'П': 'G', 'п': 'g', 'Р': 'H', 'р': 'h', 'О': 'J', 'о': 'j', 'Л': 'K', 'л': 'k', 'Д': 'L', 'д': 'l', 'Ж': ':', 'ж': ';', 'Э': '"', 'э': '\'', 'Я': 'Z', 'я': 'z', 'Ч': 'X', 'ч': 'x', 'С': 'C', 'с': 'c', 'М': 'V', 'м': 'v', 'И': 'B', 'и': 'b', 'Т': 'N', 'т': 'n', 'Ь': 'M', 'ь': 'm', 'Б': '<', 'б': ',', 'Ю': '>', 'ю': '.', ',': '?', '.': '/'}
 KEYBOARD_EN_RU = {'`': 'ё', '~': 'Ё', '@': '"', '#': '№', '$': ';', '^': ':', '&': '?', 'q': 'й', 'Q': 'Й', 'w': 'ц', 'W': 'Ц', 'E': 'У', 'e': 'у', 'R': 'К', 'r': 'к', 'T': 'Е', 't': 'е', 'Y': 'Н', 'y': 'н', 'U': 'Г', 'u': 'г', 'I': 'Ш', 'i': 'ш', 'O': 'Щ', 'o': 'щ', 'P': 'З', 'p': 'з', '{': 'Х', '[': 'х', ']': 'ъ', '}': 'Ъ', '|': '/', '\\': '\\', 'A': 'Ф', 'a': 'ф', 'S': 'Ы', 's': 'ы', 'D': 'В', 'd': 'в', 'F': 'А', 'f': 'а', 'G': 'П', 'g': 'п', 'H': 'Р', 'h': 'р', 'J': 'О', 'j': 'о', 'K': 'Л', 'k': 'л', 'L': 'Д', 'l': 'д', ':': 'Ж', ';': 'ж', '"': 'Э', '\'': 'э', 'Z': 'Я', 'z': 'я', 'X': 'Ч', 'x': 'ч', 'C': 'С', 'c': 'с', 'V': 'М', 'v': 'м', 'B': 'И', 'b': 'и', 'N': 'Т', 'n': 'т', 'M': 'Ь', 'm': 'ь', '<': 'Б', ',': 'б', '>': 'Ю', '.': 'ю', '?': ',', '/': '.'}
@@ -38,11 +39,34 @@ with codecs.open('./learn.txt', 'r', 'utf8') as f:
 print(len(voc))
 
 
+def make_ngrams(n: int, voc: set):
+    ngrams_voc = set()
+    for word in voc:
+        word_ngrams = ngrams(word, n)
+        ngrams_voc.update(word_ngrams)
+    with open(f'{n}grams.txt', 'w', encoding='utf-8') as file:
+        for ngram in ngrams_voc:
+            file.write(''.join(ngram) + '\n')
+
+
+# make_ngrams(3, voc)
+n = 3  # for ngrams
+
+ngrams_voc = set()
+with codecs.open(f'./{n}grams.txt', 'r', 'utf8') as f:
+    for line in f:
+        for word in line:
+            ngrams_voc.add(word)
+
+
 def corrector(query):
     words = query.split()
     for word in words:
         if word not in voc:
-            return translate(query)
+            word_ngrams = ngrams(word, n)
+            for ngram in word_ngrams:
+                if not ngram in ngrams_voc:
+                    return translate(query)
     return query
 
 
