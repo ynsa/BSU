be673a129113e7bc7661763639c9be44b94835eb
Yana <fairytalespring@gmail.com>
2019-04-07 11:49:12 +0300

    Finish pr5.

--- IR/IR/pr5/tests.py
+++ IR/IR/pr5/tests.py
@@ -12,15 +12,13 @@ def test_from_lecture(auc, answer):
     for i in range(3):
         ads[i].prob = 1
 
-    results = auc(ads, [1.0, 0.8])
-    result = results[1]
+    result = auc(ads, [1.0, 0.8])[1]
 
     error = max(abs(ri - ai) for ri, ai in zip(result, answer))
     if error > 1e-7:
         print(result, '!=', answer)
     else:
         print(auc.__name__, 'is fine!')
-    return results
 
 
 def test_deviant_strategy(auc, position_prob, deviant_advert_func=None,
@@ -30,15 +28,12 @@ def test_deviant_strategy(auc, position_prob, deviant_advert_func=None,
     doc_count = len(position_prob) + 1
     gen = Random(seed)
     for it in range(10000):
-        print('\n', '-' * 30)
-    # for it in range(3):
         # генерируем объявления
         ads = [Advert().init(gen) for i in range(doc_count - 1)]
         ads.append(DeviantAdvert(deviant_advert_func).init(gen))
         deviant = ads[-1]
-        print(f'Deviant get_bid: {ads[-1].get_bid(ads)}')
+
         # запускаем аукцион
-        print('F1')
         winners, costs = auc(ads, position_prob)
         assert len(winners) == len(position_prob), 'Something goes wrong'
         assert min(costs) > 0, 'No free lunch'
@@ -49,47 +44,45 @@ def test_deviant_strategy(auc, position_prob, deviant_advert_func=None,
                 # матожидание профита равно вероятности клика
                 # умноженной на ценность минус стоимость клика
                 cur_deviant_profit = (ad._value - cost) * ad.prob * p
-                print(f'get_bid for deviant = {ad.get_bid(ads)}')
                 break
 
         if deviant_advert_func is not None:
             # разыгрываем аукцион как будто девиант ведет себя честно
             deviant.get_bid_func = None
-            print('F2')
             winners_bl, costs_bl = auc(ads, position_prob)
 
             honest_deviant_profit = 0.0
             for p, (ad, cost) in zip(position_prob, zip(winners_bl, costs_bl)):
                 if ad == deviant:
                     honest_deviant_profit = (ad._value - cost) * ad.prob * p
-                    print(f'cur_deviant_profit = {cur_deviant_profit}\thonest_deviant_profit = {honest_deviant_profit}')
                     break
 
             # если честный профит меньше нечестного - фейлим тест
-            if cur_deviant_profit > honest_deviant_profit:
-                print(f'{cur_deviant_profit - honest_deviant_profit}')
+            if cur_deviant_profit > honest_deviant_profit and \
+                    abs(cur_deviant_profit - honest_deviant_profit) > pow(10, -13):
                 if debug:
                     print('Fail')
-                    print(f'n = {len(position_prob)}')
-                    print(f'position_prob: {", ".join([str(x) for x in position_prob])}')
+                    print('Difference between profits: '
+                          f'{cur_deviant_profit - honest_deviant_profit}')
+
                     print('Deviant auction results:')
-                    for i, ad in enumerate(ads):
+                    for ad in ads:
                         try:
                             idx = winners.index(ad)
-                            print(f'Ad {i}:', ad, 'Win:', idx, 'Bid:',
-                                  ad.get_bid(ads), 'Cost:',
+                            print('Ad:', ad, 'Win:', idx, 'Bid:',
+                                  deviant_advert_func(ad, ads), 'Cost:',
                                   costs[idx])
                         except:
-                            print(f'Ad {i}:', ad)
+                            print('Ad:', ad)
                     print()
                     print('Honest auction results:')
-                    for i, ad in enumerate(ads):
+                    for ad in ads:
                         try:
                             idx = winners_bl.index(ad)
-                            print(f'Ad {i}:', ad, 'Win:', idx, 'Bid:',
-                                  ad.get_bid(ads), 'Cost:', costs[idx])
+                            print('Ad:', ad, 'Win:', idx, 'Bid:',
+                                  Advert.get_bid(ad, ads), 'Cost:', costs[idx])
                         except:
-                            print(f'Ad {i}:', ad)
+                            print('Ad:', ad)
                     print()
                     print()
 
@@ -112,11 +105,6 @@ def create_eps_pos_strategy(pos_delta, eps):
         return max(winners[idx]._value * winners[idx].prob / self.prob + eps, 1e-5)
     return minus_eps_pos_bid
 
-def my_str():
-    def nyam(self, ads):
-        return 5
-    return nyam
-
 
 STRATEGIES = [
     create_eps_strategy(+0.5),
@@ -131,15 +119,13 @@ STRATEGIES = [
     create_eps_pos_strategy(-5, +1e-5),
     create_eps_pos_strategy(+5, -1e-5),
     create_eps_pos_strategy(-5, -1e-5),
-    my_str()
 ]
 
 
 def test_deviant_strategies(aucs):
     # пример вероятностей клика по позиции
     # можно менять на своё усмотрение
-    # position_prob = [0.9, 0.7, 0.5, 0.4]
-    position_prob = [1, 0.8]
+    position_prob = [0.9, 0.7, 0.5, 0.4]
     for auc in aucs:
         for si, strategy in enumerate(STRATEGIES):
             for n in range(1, len(position_prob) + 1):
@@ -149,54 +135,6 @@ def test_deviant_strategies(aucs):
                     strategy_name = strategy.__name__
                     print('Auction `%s` is cheated with strategy `%d: %s` with profit %s' % (auc_name, si, strategy_name, profit))
     print('Done')
-
-
-def my_test(auc, answer):
-    # winners_bl, costs_bl = test_from_lecture(auc, answer)
-
-    ads = [Advert() for i in range(3)]
-    ads[0]._value = 10
-    ads[1]._value = 4
-    ads[2]._value = 2
-    for i in range(3):
-        ads[i].prob = 1
-
-    position_prob = [1.0, 0.8]
-    winners_bl, costs_bl = auc(ads, position_prob)
-
-    for p, (ad, cost) in zip(position_prob, zip(winners_bl, costs_bl)):
-        if ad._value == ads[2]._value:
-            honest_deviant_profit = (ad._value - cost) * ad.prob * p
-            print(f'Honest: {honest_deviant_profit}')
-            break
-
-    # for si, strategy in enumerate(STRATEGIES):
-    # ads = [Advert() for i in range(3)]
-    ads[0]._value = 10
-    ads[1]._value = 4
-    ads[2] = DeviantAdvert(STRATEGIES[-1])
-    ads[2]._value = 2
-    for i in range(3):
-        ads[i].prob = 1
-
-
-    # result = auc(ads, [1.0, 0.8])[1]
-    winners_bl, costs_bl = auc(ads, position_prob)
-
-    honest_deviant_profit = 0.0
-    for p, (ad, cost) in zip(position_prob, zip(winners_bl, costs_bl)):
-        if ad._value == ads[2]._value:
-            honest_deviant_profit = (ad._value - cost) * ad.prob * p
-            print(f'Not honest: {honest_deviant_profit}')
-            break
-
-        # if result[1] < honest_res[1]:
-        #     print(result[1], '<', honest_res[1])
-        # error = max(abs(ri - ai) for ri, ai in zip(result, answer))
-        # if error > 1e-7:
-        #     print(result, '!=', answer)
-        # else:
-        #     print(auc.__name__, 'is fine!')
 
 
 if __name__ == '__main__':
--- IR/IR/pr5/vcg.py
+++ IR/IR/pr5/vcg.py
@@ -1,6 +1,6 @@
 class Advert(object):
     def init(self, gen):
-        self.prob = round(gen.uniform(0.1, 1), 1)
+        self.prob = gen.random()
         self._value = gen.randint(1, 10) * 9 + 1
         return self
 
@@ -45,43 +45,13 @@ def auction_vcg(ads, position_prob):
     winners = sorted(ads, key=lambda ad: ad.prob * ad.get_bid(ads), reverse=True)[:n + 1]
     costs = [0] * n
 
-    # if type(winners[0]) == DeviantAdvert and winners[0].get_bid_func:
-    #     print(f'\tWinner: {winners[0]}\tdeviant winner combination: {winners[0].prob * winners[0].get_bid(ads)}')
-
     gold_positions = sum(map(lambda x: x[0].get_bid(ads) * x[1] * x[0].prob, zip(winners, position_prob)))
-    # if n == 2:
-        # costs[0] = winners[2].get_bid(ads) * winners[2].prob / winners[0].prob
-        # costs[1] = winners[2].get_bid(ads) * winners[2].prob / winners[1].prob
-        # costs[0] = (position_prob[0] * winners[1].get_bid(ads) + position_prob[1] * winners[2].get_bid(ads) - position_prob[1] * winners[1].get_bid(ads)) / position_prob[0]
-        # costs[1] = winners[2].get_bid(ads)
-    # elif n == 3:
-    #     costs[0] = winners[3].get_bid(ads) * winners[3].prob / winners[0].prob
-    #     costs[1] = winners[3].get_bid(ads) * winners[3].prob / winners[1].prob
-    #     costs[2] = winners[3].get_bid(ads) * winners[3].prob / winners[2].prob
-        # costs[0] = (position_prob[0] * winners[1].get_bid(ads) + position_prob[1] * winners[2].get_bid(ads) + position_prob[2] * winners[3].get_bid(ads)
-        #             - position_prob[1] * winners[1].get_bid(ads) - position_prob[2] * winners[2].get_bid(ads)) / position_prob[0]
-        # costs[1] = (position_prob[1] * winners[2].get_bid(ads) + position_prob[1] * winners[3].get_bid(ads) - position_prob[2] * winners[2].get_bid(ads)) / position_prob[1]
-        # costs[2] = winners[3].get_bid(ads)
-
-    # elif n == 3:
-    # for i, winner in enumerate(winners[:-1]):
-    #     unreal_winners = winners.copy()
-    #     unreal_winners.pop(i)
-    #     m = sum(map(lambda x: x[0].get_bid(ads) * x[1], zip(unreal_winners, position_prob)))
-    #     costs[i] = (m - (gold_positions - position_prob[i] * winner.get_bid(ads))) / position_prob[i]
-    #     # print(costs[i])
-
-    # else:
-    #     # print(f'n = {n}')
-    #     # costs[0] = winners[1].get_bid(ads)
     for i, winner in enumerate(winners[:-1]):
-        print(f'\tWinner: {winner}\tget_bid: {winner.get_bid(ads)}\n\tdeviant winner combination: {winner.prob * winner.get_bid(ads)}')
         unreal_winners = winners.copy()
         unreal_winners.pop(i)
         m = sum(map(lambda x: x[0].get_bid(ads) * x[1] * x[0].prob, zip(unreal_winners, position_prob)))
-        costs[i] = (m - (gold_positions - position_prob[i] * winner.get_bid(ads) * winner.prob)) / position_prob[i]
-        # if type(winner) == DeviantAdvert:
-        #     print(f'\tdeviant winner combination: {winner.prob * winner.get_bid(ads)}')
-            # print(costs[i])
+
+        winner_weight = position_prob[i] * winner.prob
+        costs[i] = (m - (gold_positions - winner.get_bid(ads) * winner_weight)) / winner_weight
     del winners[-1]
     return winners, costs
